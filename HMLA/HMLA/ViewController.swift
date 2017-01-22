3843b9bd325e2b70f8aa4d1a579039d2c1b1d970
Jack_lin <401788217@qq.com>
2017-01-22 23:33:12 +0800

    init HMLA project in swift 2.3

---
+++ HMLA/HMLA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HMLA
+//
+//  Created by Sunxiaolin on 17/1/22.
+//  Copyright © 2017年 com.jack.lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
