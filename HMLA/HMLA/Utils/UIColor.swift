6f27aec5b3e57951d6cf0477faee1b25a24492e3
Jack_lin <401788217@qq.com>
2017-01-23 11:36:48 +0800

    添加颜色的Hex方法

---
+++ HMLA/HMLA/Utils/UIColor.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor.swift
+//  XCTG3
+//
+//  Created by LTHJ on 2017/1/12.
+//  Copyright © 2017年 lthj. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    
+    class func BackColor()-> UIColor{
+        return UIColor(hexString: "#efeff4")!
+    }
+    
+    class func LineColor()-> UIColor{
+        return UIColor(hexString: "#d7d7d7")!
+    }
+    
+}
